fc7ced91b994640a974fee6f0d5cbb9cd97004ad
srafaga0 <38598814+srafaga0@users.noreply.github.com>
2023-03-16 09:00:40 -0500

    import openyxl

--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,6 @@
-#importar la libreria tkinter
+#importar las librerias 
 from tkinter import *
-
+from openpyxl import * 
 #creacion de ventana
 ventana= Tk()
 ventana.geometry("200x60+550+100")
@@ -14,4 +14,6 @@ text_inicial.place(x=50, y=10)
 #agregamos cuadro de entrada
 entrada_text= Entry(ventana,font="arial 9")
 entrada_text.place(x=33,y= 30)
+
+
 ventana.mainloop()
