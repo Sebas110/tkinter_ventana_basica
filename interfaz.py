8e3bbb5e93aeb3640f8776fa9e6ca7d6c5933958
srafaga0 <38598814+srafaga0@users.noreply.github.com>
2023-03-16 09:06:33 -0500

    cambiamos el tamaño de la ventana

--- interfaz.py
+++ interfaz.py
@@ -3,7 +3,7 @@ from tkinter import *
 from openpyxl import * 
 #creacion de ventana
 ventana= Tk()
-ventana.geometry("200x60+550+100")
+ventana.geometry("200x100+550+100")
 ventana.config(bg="black")
 ventana.title("ventana")
 
@@ -13,7 +13,9 @@ text_inicial.place(x=50, y=10)
 
 #agregamos cuadro de entrada
 entrada_text= Entry(ventana,font="arial 9")
-entrada_text.place(x=33,y= 30)
+entrada_text.place(x=33,y= 35)
 
-
+#agregamos boton
+boton1= Button(text="OK")
+boton1.place(x=90,y=60)
 ventana.mainloop()
