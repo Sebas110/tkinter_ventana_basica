0f29ebf97a2b0903b24f47861175de24c127a8ac
srafaga0 <38598814+srafaga0@users.noreply.github.com>
2023-03-16 08:43:20 -0500

    agregar texto inicial

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,15 @@
+#importar la libreria tkinter
 from tkinter import *
 
+#creacion de ventana
 ventana= Tk()
 ventana.geometry("200x200+550+100")
 ventana.config(bg="black")
 ventana.title("ventana")
+
+#texto inicial
+text_inicial= Label(ventana,text=("ingrese nombre"),bg="black",fg="white",font="arial 11")
+text_inicial.place(x=50, y=10)
+
+
 ventana.mainloop()
